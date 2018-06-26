50bf24b5e0736f0e09fafbadb71666eec03ca902
Tarash Agarwal <tarash.agarwal@voylla.com>
2018-06-27 02:47:41 +0530

    Progress

--- lib/skynet.c
+++ lib/skynet.c
@@ -5,8 +5,9 @@ VALUE create_hash() {
   return rb_str_new2("test");
 }
 
-VALUE distance() {
-  return rb_str_new2("test");
+VALUE distance(VALUE self,VALUE str) {
+  char *st = (char*)(StringValuePtr(str)); 
+  return rb_str_new2(st);
 }
 
 VALUE rb_return_nil() {
@@ -18,7 +19,7 @@ void Init_skynet()
  
   rb_define_method(mod, "return_nil", rb_return_nil, 0);
   rb_define_method(mod, "create_hash", create_hash, 0);
-  rb_define_global_function("distance",distance,0);
+  rb_define_global_function("distance",distance,1);
 }
 
 int main()
