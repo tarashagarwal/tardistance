fc85ffa313b041c24d5fb2700133b17215da3e20
Tarash Agarwal <tarash.agarwal@voylla.com>
2018-07-08 19:04:36 +0530

    Parsed ruby array to c

--- lib/skynet.c
+++ lib/skynet.c
@@ -1,13 +1,32 @@
 #include <ruby.h>
 #include "extconf.h"
 
+char **getWordsArray(VALUE rarray);
+char *getString(void);
+
 VALUE create_hash() {
   return rb_str_new2("test");
 }
 
-VALUE distance(VALUE self,VALUE str) {
-  char *st = (char*)(StringValuePtr(str)); 
-  return rb_str_new2(st);
+VALUE distance(VALUE self,VALUE str,VALUE rarray) {
+	int arrayLen =  RARRAY_LEN(rarray);
+	int stringLength = RSTRING_LEN(str);
+	int i =0;
+	char *res[1000];
+	if(!NIL_P(rarray) && arrayLen > 0 && arrayLen < 1000 && stringLength > 0 && stringLength <= 100){
+		while(i < arrayLen){
+			VALUE element = rb_ary_entry(rarray,i);
+			res[i] = StringValuePtr(element);
+			i++;
+		}
+		// char *st = (char*)(StringValuePtr(str1));
+		// VALUE tt = rb_ary_entry(rarray,0);
+		// int len = RSTRING_LEN(tt);
+		// st = (char*)(StringValuePtr(len)); 
+	}else{
+		//raise error
+	}
+	return rb_str_new2(res[0]);
 }
 
 // VALUE fonk(int argc,VALUE* argv,VALUE self) {
@@ -22,11 +41,42 @@ VALUE distance(VALUE self,VALUE str) {
 
 VALUE bulkDistance(VALUE self,VALUE rstr,VALUE rarray) {
 	//int arrayLen = RARRAY_LEN(rarray);
-	VALUE rfirstElement = rb_ary_entry(rarray,0);
-	int eleLength = RSTRING_LEN(rfirstElement);
-	char snum[100];
-	sprintf(snum,"%d",eleLength); 
-	return rb_str_new2(snum);
+	char **word_list;
+	word_list = (char**)getWordsArray(rarray);
+	//VALUE rfirstElement = rb_ary_entry(rarray,0);
+	//int eleLength = RSTRING_LEN(rfirstElement);
+	//char snum[100];
+	//sprintf(snum,"%d",eleLength);
+	//return rb_str_new2(getString());
+	//return rb_str_new2("hello");
+	return rb_str_new2(*word_list);
+}
+
+char *getString(void){
+    char *str;
+    str = (char*)"Hello";
+	return str;
+}
+
+char **getWordsArray(VALUE rarray){
+	char **res;
+	int i =0;
+	res = NULL;
+	if(!NIL_P(rarray)){
+		int arrayLen = RARRAY_LEN(rarray);
+		if(arrayLen <= 1000){
+			//accepting only 1000 words array at the moment
+			while(i < arrayLen){
+				VALUE element = rb_ary_entry(rarray,i);
+				int eleLength = RSTRING_LEN(element);
+				if(eleLength <= 100){
+					*(res + i) = (char*)StringValuePtr(element);
+				}
+				i++;
+			}
+		}
+	}
+	return res;
 }
 
 VALUE rb_return_nil() {
@@ -38,7 +88,7 @@ void Init_skynet()
  
   rb_define_method(mod, "return_nil", rb_return_nil, 0);
   rb_define_method(mod, "create_hash", create_hash, 0);
-  rb_define_global_function("distance",distance,1);
+  rb_define_global_function("distance",distance,2);
   //rb_define_global_function("fonk",fonk,2);
   rb_define_global_function("get_distance",bulkDistance,2);
 }
