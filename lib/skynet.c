628ddb67a2d82be01c682be4bac4f102bd96e0d3
Tarash Agarwal <tarash.agarwal@voylla.com>
2018-07-10 00:10:05 +0530

    finally adding levensteing algorithm

--- lib/skynet.c
+++ lib/skynet.c
@@ -3,7 +3,11 @@
 
 char **getWordsArray(VALUE rarray);
 char *getString(void);
-double findDistance(char*,char*);
+double findDistance(char*,char*,int,int);
+int min(int,int,int);
+int findMin(int,int);
+int k = 0 ,l = 0;
+int dp[1001][101];
 
 VALUE create_hash() {
   return rb_str_new2("test");
@@ -17,21 +21,27 @@ VALUE distance(VALUE self,VALUE str,VALUE rarray,VALUE cutoff) {
 	long finalCount = 0;
 	char *searchWord;
 	char *data[1000];
-	double  cutoffScore = NUM2DBL(cutoff);
+	int dataLen[1000];
 	VALUE finalResult[1000];
-	if(NIL_P(rarray) || cutoffScore < 0){
-		cutoffScore= 0.5;
+	double  cutoffScore = -1.0;
+	if(!NIL_P(cutoff)){
+		cutoffScore = NUM2DBL(cutoff);
 	}
-	if(!NIL_P(rarray) && arrayLen > 0 && arrayLen < 1000 && stringLength > 0 && stringLength <= 100){
+	if(cutoffScore < 0.0){
+		cutoffScore = 0.5;
+	}
+	if(!NIL_P(rarray) && arrayLen > 0 && arrayLen <= 1000 && stringLength > 0 && stringLength <= 100){
 		while(i < arrayLen){
 			VALUE element = rb_ary_entry(rarray,i);
 			data[i] = StringValuePtr(element);
+			dataLen[i] = RSTRING_LEN(element);
 			i++;
 		}
 		searchWord = (char*)StringValuePtr(str);
 		while(j<i){
-			if(findDistance(data[j],searchWord) <= cutoffScore){
-				finalResult[finalCount++] = rb_str_new_cstr(data[j]);
+			if(findDistance(data[j],searchWord,dataLen[j],stringLength) <= cutoffScore){
+				finalResult[finalCount] = rb_str_new_cstr(data[j]);
+				finalCount++;
 			}
 			j++;
 		}
@@ -48,14 +58,53 @@ VALUE distance(VALUE self,VALUE str,VALUE rarray,VALUE cutoff) {
 // VALUE fonk(int argc,VALUE* argv,VALUE self) {
 //   VALUE str;
 //   VALUE block;
-//   rb_scan_args(argc, argv, "1&", &str,&block);
+//   rb_scan_args(argc, arfinalResultgv, "1&", &str,&block);
 //   if (NIL_P(block)) block = rb_ary_new();
 //   char *st = (char*)(StringValuePtr(str)); 
 //   return rb_str_new2(st);
 // }
 
-double findDistance(char* dataStr,char* searchWord){
-	return 0.0;
+double findDistance(char* dataStr,char* searchWord,int dataLen,int searchWordLen){
+	if (dataLen <= 1000 && searchWordLen <= 100){
+		char *a =dataStr;
+		char *b =searchWord;
+	   	for (k=0; k<=dataLen; k++){
+	   		b =searchWord;
+	        for (l=0; l<=searchWordLen; l++){
+	            if (k==0){
+	                dp[k][l] = l;
+	            }
+	            else if (l==0){
+	                dp[k][l] = k;
+	            }
+	            else if ( *a == *b){
+	                dp[k][l] = dp[k-1][l-1];
+	            }
+
+	            else{
+	                dp[k][l] = 1 + min(dp[k][l-1],  // Insert
+	                                   dp[k-1][l],  // Remove
+	                                   dp[k-1][l-1]); // Replace
+	            }
+	            b++;
+	        }
+	        a++;
+	    }
+	}
+	 
+    return ((double)dp[k-1][l-1]/searchWordLen);
+}
+
+int min(int x,int y,int z){
+	return findMin(x,findMin(x,y));
+}
+int findMin(int r,int s){
+	if (r < s){
+		return r;
+	}
+	else{
+		return s;
+	}
 }
 
 VALUE bulkDistance(VALUE self,VALUE rstr,VALUE rarray) {
